17bfbaf9d6d55f73972e4dabd34a23bd8e8c8565
Carlos Gonzalez <carlosgj94@gmail.com>
2017-01-21 18:03:32 +0200

    Added waves temporaly and CHanges on some prefabs

---
+++ Scripts/Wave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector2 jumpForceUp = new Vector2(-7.0f, 0.0f);
+        this.GetComponent<Rigidbody2D>().AddForce(jumpForceUp, ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Scripts/WaveInstantiator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveInstantiator : MonoBehaviour {
+    public GameObject wave1;
+    public GameObject wave2;
+    public GameObject wave3;
+
+    public float period;
+    float TimeInterval = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeInterval += Time.deltaTime;
+        if (TimeInterval >= period)
+        {
+            TimeInterval = 0;
+            Vector3 savePosition = new Vector3(GetComponent<Transform>().position.x - 2, GetComponent<Transform>().position.y + 1, GetComponent<Transform>().position.z);
+            int rnd = Random.Range(0, 4);
+            if (rnd >= 3)
+                Instantiate(wave1, savePosition, Quaternion.identity);
+            else if (rnd >= 2)
+                Instantiate(wave2, savePosition, Quaternion.identity);
+            else
+                Instantiate(wave3, savePosition, Quaternion.identity);
+        }
+    }
+}
