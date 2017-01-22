6136f48e5ba1ec74dbdb591490f9a6e4eb0b23f4
Carlos Gonzalez <carlosgj94@gmail.com>
2017-01-22 06:11:36 +0200

    Intro scene done, Scene0 done also. Added the icons of the team

---
+++ Scripts/StarWarsText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StarWarsText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector2 jumpForceUp = new Vector2(0.0f, 0.6f);
+        this.GetComponent<Rigidbody2D>().AddForce(jumpForceUp, ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Finish")
+        {
+            Debug.Log("Inside");
+            SceneManager.LoadScene("Scene1");
+        }
+    }
+}
